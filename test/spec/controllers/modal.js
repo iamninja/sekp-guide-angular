339790b93c77bb0c03162411c2dbc6cbe6167ac8
Vagios Vlachos <bagios234@gmail.com>
2015-06-09 07:46:33 +0300

    Remove assertions from tests

--- test/spec/controllers/modal.js
+++ test/spec/controllers/modal.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: ModalctrlCtrl', function () {
+describe('Controller: ModalCtrl', function () {
 
   // load the controller's module
   beforeEach(module('sekpGuideApp'));
 
-  var ModalctrlCtrl,
+  var ModalCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    ModalctrlCtrl = $controller('ModalctrlCtrl', {
+    ModalCtrl = $controller('ModalCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(scope.awesomeThings.length).toBe(3);
+  // });
 });
