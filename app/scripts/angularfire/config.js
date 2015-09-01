6765fe9b3ebbc73ced0dee6851cf556c44c7cee1
Vagios Vlachos <bagios234@gmail.com>
2015-09-01 22:44:19 +0300

    Prepare for github pages

--- app/scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://sekp-guide.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','anonymous'])
-
-  .constant('loginRedirectPath', '/login');
--- app/scripts/filters/reverse.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('sekpGuideApp')
-  .filter('reverse', function() {
-    return function(items) {
-      return angular.isArray(items)? items.slice().reverse() : [];
-    };
-  });
